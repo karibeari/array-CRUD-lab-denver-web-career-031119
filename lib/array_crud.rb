415e982cebe1b439067ac18061f2443e5d178c33
github username <github email address>
2019-02-19 12:56:29 -0700

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,35 @@
 def create_an_empty_array
-  
+  new_array = []
 end
 
 def create_an_array
-  
+  ski_resorts = ["Keystone", "Winter Park", "Breckenridge", "Taos"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array[-1]
 end
